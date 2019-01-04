0acbadc0f9084a7ef81ecf55755cb949f6bf7c69
Fippiy <profier4@gmail.com>
2019-01-04 12:51:44 +0900

    Edit routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # root 'messages#index'
   root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :edit, :create, :update] do
+  resources :groups, only: [:index, :new, :edit, :create, :update] do
     resources :messages, only: [:index, :create]
   end
 end
