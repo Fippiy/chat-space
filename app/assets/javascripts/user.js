ded16ad710c841cfc6dbf9833776fd0aea386595
Fippiy <profier4@gmail.com>
2019-01-07 19:10:00 +0900

    Edit user.js

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,9 +1,8 @@
 $(function() {
 
-  var user_list = $("#chat-group-users");
+  var user_list = $("#user-search-result");
 
   function appendUser(user) {
-    // var html = `<div>${ user.nickname }</div>`
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.nickname }</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.nickname }">追加</a>
@@ -18,10 +17,17 @@ $(function() {
     user_list.append(html);
   }
 
+  function appendAddUser(userId, userNickname) {
+    var addHtml = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${ userId }'>
+                    <input name='group[user_ids][]' type='hidden' value='${ userId }'>
+                    <p class='chat-group-user__name'>${ userNickname }</p>
+                    <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                  </div>`
+    $('.chat-group-users').append(addHtml);
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $(this).val();
-    console.log(input);
-
     $.ajax({
       type: 'GET',
       url: '/users',
@@ -29,9 +35,7 @@ $(function() {
       dataType: 'json'
     })
     .done(function(users) {
-      console.log(users);
-      // 同じで使えるなら、先頭の配列を利用する
-      $("#chat-group-users").empty();
+      user_list.empty();
       if (users.length !== 0) {
         users.forEach(function(user) {
           appendUser(user);
@@ -44,5 +48,15 @@ $(function() {
     .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+  });
+
+  user_list.on("click", ".user-search-add", function() {
+    var userId = $(this).attr('data-user-id');
+    var userNickname = $(this).attr('data-user-name');
+    $(this).parent().remove();
+    appendAddUser(userId, userNickname);
+  });
+  $(".chat-group-users").on("click", ".user-search-remove", function() {
+    $(this).parent().remove();
   });
 });
