2db90c1bb7e2832ba4991374810794eeb75c7e6a
Fippiy <profier4@gmail.com>
2019-01-07 13:00:39 +0900

    edit users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.where('nickname LIKE(?)', "%#{params[:keyword]}%")
+    # render: json
+    respond_to do |format|
+      format.json
+    end
+  end
+
   def edit
   end
 
