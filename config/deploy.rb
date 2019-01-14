20f9e200337efc45e88e9673db2d04bd1afb121a
Fippiy <profier4@gmail.com>
2019-01-14 13:15:56 +0900

    Edit deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "chat-space"
-set :repo_url, "git@example.com:me/Fippiy/chat-space.git"
+set :repo_url, "git@github.com:me/Fippiy/chat-space.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
