05f2f636eca7b1061db0297c5b435752548ad89f
Fippiy <profier4@gmail.com>
2019-01-14 18:26:02 +0900

    Edit js test

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -6,17 +6,6 @@ $(function() {
     var html_body =
       message.image == null ? message.text : message.text + "<img src=" + message.image + ">";
     var html = "test"
-     // `<div class="  main__body--box clearfix" message_id="${ message.id }">
-     //              <li class="message-nickname">
-     //                ${ message.nickname }
-     //              </li>
-     //              <li class="message-date">
-     //                ${ message.created_at }
-     //              </li>
-     //              <li class="message-text">
-     //                ${ html_body }
-     //              </li>
-     //            </div>`
     message_list.append(html);
     $('html, body').scrollTop($(document).height());
   }
